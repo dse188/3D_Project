284a86d7fffb2ef034d4f69731782bd643d92b6f
Derman Cetinalp <derman.cetinalp@gmail.com>
2024-06-17 04:32:59 -0400

    Sample movement
    - Player move towards the direction it is facing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
     [SerializeField] CharacterController cc;
-    Vector3 input;
-    [SerializeField] Transform feetPosition;
-    [SerializeField] LayerMask groundLayer;
-    [SerializeField] bool playerGrounded;
-
-    //Jump stuffs
-    [SerializeField] float jumpPower;
-    [SerializeField] bool jump;
-    Vector3 playerVector;
-    [SerializeField] float gravityValue = -9.81f;
+    Vector3 input = Vector3.zero;
 
     private void Start()
     {
@@ -23,51 +14,30 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         MoveInput();
-        JumpInput();
     }
 
     private void FixedUpdate()
     {
-        Move();
-        Jump();
+        MovePlayer();
+        RotatePlayer();
     }
 
     void MoveInput()
     {
-        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        //input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        input = new Vector3(0, 0, Input.GetAxisRaw("Vertical")).normalized;
 
     }
 
-    void JumpInput()
-    {
-        if(Input.GetKeyDown("Jump"))
-        {
-            jump = true;
-        }
-        if (!IsGrounded())
-        {
-            jump = false;
-        }
-    }
-
-    void Move()
+    void MovePlayer()
     {
         //rb.velocity = input * moveSpeed;
+        input = transform.TransformDirection(input);
         cc.SimpleMove(input * moveSpeed);
     }
 
-    void Jump()
+    void RotatePlayer()
     {
-        if(IsGrounded() && jump)
-        {
-            //cc.SimpleMove(new Vector3(Input.GetAxisRaw("Horizontal"), jumpPower, Input.GetAxisRaw("Vertical")).normalized);
-            playerVector.y += Mathf.Sqrt(jumpPower * -3.0f * gravityValue);
-        }
-    }
-
-    bool IsGrounded()
-    {
-        return cc.isGrounded;
-        //return playerGrounded = cc.isGrounded;
+        transform.Rotate(0, Input.GetAxisRaw("Horizontal") * 10, 0);
     }
 }
